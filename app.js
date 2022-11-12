d7e0de2a7122ad3aa5320299da85534e8553ebec
Matthew Trent <matthew@Matts-Macbook-Pro.local>
2022-11-12 01:12:58 -0800

    added cors

--- app.js
+++ app.js
@@ -5,10 +5,9 @@ const axios = require('axios');
 const { create_tags } = require('./utils/create_tags');
 const { create_ingredients } = require('./utils/create_ingredients');
 const { create_name } = require('./utils/create_name');
+var cors = require('cors');
 
-
-
-
+app.use(cors());
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
